ac67e026d5887e2ce2e47692e1e8e1106c38a656
Vladimir RM <shvdrummer@gmail.com>
2023-02-07 09:36:11 +0200

    add const todo

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { addTodo } from "./todoSlice";
 
 const Form = () => {
+const dispatch = useDispatch( )
+
+const addTodoHandler =()=>{
+    const todo ={
+        id: '',
+        text:'',
+        completed: 
+    }
+}
+
   return <form onSubmit={(e) => e.preventDefault()}>
     <input type="text"
     placeholder="Type something"
