1093dbfabec2d80282f99cdc6f9f344ed13cea9a
Vladimir RM <shvdrummer@gmail.com>
2023-02-07 11:15:15 +0200

    add slice removeTodo

--- src/componets/TodoItem.js
+++ src/componets/TodoItem.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React from "react";
+import { useDispatch } from "react-redux";
+import toggleCompletedTodo from "./todoSlice";
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const toggleTodoHandler = (id) => {
+    dispatch(toggleCompletedTodo(id));
+  };
 
-    return (
-        <div className='flex justify-between items-center my-2'>
-            <div className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'>
-                Complete
-            </div>
-            <div className={`text-sm ${todo.completed ? 'line-through font-medium text-lime-400' : ''}`}>
-                Todo Text
-            {todo.text}
-            </div>
-            <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
-                Delete
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-between items-center my-2">
+      <div  onClick={()=>toggleTodoHandler(todo.id)}
+      className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400">
+        Complete
+      </div>
+      <div
+        className={`text-sm ${
+          todo.completed ? "line-through font-medium text-lime-400" : ""
+        }`}
+      >
+        Todo Text
+        {todo.text}
+      </div>
+      <div className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer">
+        Delete
+      </div>
+    </div>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
--- src/componets/TodoSlice.js
+++ src/componets/TodoSlice.js
@@ -1,21 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-     todos:[],
-}
-  export const  todoSlice = createSlice({
-    name:'todos',
-    initialState,
-    reducers:{
-        addTodo:(state,action)=>{
-            state.todos.push(action.payload)
-        } ,
-        toggleCompletedTodo:(state,action)=>{
-                 const toggleTodo =state.todos.find((todo)=>todo.id===action.payload)
-                toggleTodo.completed = !toggleTodo.completed        }
+const initialState = {
+  todos: [],
+};
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
     },
-  })
+    toggleCompletedTodo: (state, action) => {
+      const toggleTodo = state.todos.find((todo) => todo.id === action.payload);
+      toggleTodo.completed = !toggleTodo.completed;
+    },
+    removeTodo: (state,action)
+  },
+});
 
-  export const {addTodo}= todoSlice.actions
-  export default todoSlice.reducer
-
+export const { addTodo, toggleCompletedTodo } = todoSlice.actions;
+export default todoSlice.reducer;
