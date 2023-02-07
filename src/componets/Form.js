465a9069e3580a77f1f233fd45730f0e09758dce
Vladimir RM <shvdrummer@gmail.com>
2023-02-07 09:30:15 +0200

    add input

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Form = () => {
   return <form onSubmit={(e) => e.preventDefault()}>
+    <input type="text"
+    placeholder="Type something"
     
+    />
+    <button type="submit"></button>
   </form>;
 };
 
