ead4f522cd0cbcf8f4b7f74717b3d9b454d4a872
Vladimir RM <shvdrummer@gmail.com>
2023-02-08 11:54:22 +0200

    update code in Form

---
+++ src/componets/todoSlice1.js
@@ -0,0 +1,20 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   todos:[]
+  
+// }
+
+
+// export const todoSlice =createSlice({
+//      name:'todos',
+//      initialState,
+//      reducers:{
+//       addTodo:(state,action)=>{
+
+//       }
+//      }
+// })
+
+// export const {addTodo }=todoSlice.actions
+// export default todoSlice.reducer
