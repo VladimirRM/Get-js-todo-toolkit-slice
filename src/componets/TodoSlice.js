fa49031c53b3037e476330f2927159443a26bcb5
Vladimir RM <shvdrummer@gmail.com>
2023-02-08 11:42:36 +0200

    create addTodo slice

--- src/componets/TodoSlice.js
+++ src/componets/TodoSlice.js
@@ -7,5 +7,14 @@ const initialState = {
 
 
 export const todoSlice =createSlice({
-  
+     name:'todos',
+     initialState,
+     reducers:{
+      addTodo:(state,action)=>{
+
+      }
+     }
 })
+
+export const {addTodo }=todoSlice.actions
+export default todoSlice.reducer
